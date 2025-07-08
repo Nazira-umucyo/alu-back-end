3149670919aecc4756a6b767ba56e9f8d7016704
Nazira-umucyo <n.umucyo@alustudent.com>
2025-07-08 23:57:07 +0200

    whyyy

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -7,27 +7,27 @@ import sys
 
 
 if __name__ == "__main__":
-        """ main section """
-            EMP_ID = sys.argv[1]
-                BASE_URL = 'https://jsonplaceholder.typicode.com'
-                    employee = requests.get(
-                                    BASE_URL + f'/users/{EMP_ID}/').json()
-                        EMPLOYEE_NAME = employee.get("username")
-                            employee_todos = requests.get(
-                                            BASE_URL + f'/users/{EMP_ID}/todos').json()
-                                serialized_todos = {}
+    """ main section """
+    EMP_ID = sys.argv[1]
+    BASE_URL = 'https://jsonplaceholder.typicode.com'
+    employee = requests.get(
+        BASE_URL + f'/users/{EMP_ID}/').json()
+    EMPLOYEE_NAME = employee.get("username")
+    employee_todos = requests.get(
+        BASE_URL + f'/users/{EMP_ID}/todos').json()
+    serialized_todos = {}
 
-                                    for todo in employee_todos:
-                                                serialized_todos.update({todo.get("title"): todo.get("completed")})
+    for todo in employee_todos:
+        serialized_todos.update({todo.get("title"): todo.get("completed")})
 
-                                                    COMPLETED_LEN = len([k for k, v in serialized_todos.items() if v is True])
-                                                        with open(str(EMP_ID) + '.csv', "w") as f:
-                                                                    [
-                                                                                        f.write(
-                                                                                                            '"' + str(sys.argv[1]) + '",' +
-                                                                                                                            '"' + EMPLOYEE_NAME + '",' +
-                                                                                                                                            '"' + str(todo["completed"]) + '",' +
-                                                                                                                                                            '"' + todo["title"] + '",' + "\n"
-                                                                                                                                                                        )
-                                                                                                    for todo in employee_todos
-                                                                                                            ]
+    COMPLETED_LEN = len([k for k, v in serialized_todos.items() if v is True])
+    with open(str(EMP_ID) + '.csv', "w") as f:
+        [
+            f.write(
+                '"' + str(sys.argv[1]) + '",' +
+                '"' + EMPLOYEE_NAME + '",' +
+                '"' + str(todo["completed"]) + '",' +
+                '"' + todo["title"] + '",' + "\n"
+            )
+            for todo in employee_todos
+        ]
